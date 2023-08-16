e99461ccbf1fb851e7531925186bd6f87e292e7a
chandrika-kp <102351627+chandrika-kp@users.noreply.github.com>
2023-08-16 21:53:27 +0530

    Update ViewEachProduct.js

--- src/components/HomePage/ViewEachProduct.js
+++ src/components/HomePage/ViewEachProduct.js
@@ -1,72 +1,82 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import "../../App.css";
-import { NavLink, useParams } from "react-router-dom";
-import "./MyCartitems.json"
-import SelectedProducts from './SelectedProducts';
+import { NavLink } from "react-router-dom";
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
-let selectedList = []
 
-const ViewEachProduct = () => {
+let categorylist = []
 
-  const [eachProducts, setEachProducts] = useState({
-    image: "",
-    category: "",
-    title: "",
-    price: "",
-    description: "",
-    rating: {
-      rate: "",
-      count: ""
-    }
-  });
-
-  const { id } = useParams();
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     getProductsData()
   }, []);
 
   const getProductsData = async () => {
-    const productsJsonData = await axios.get("http://localhost:3002/products/" + id)
-    setEachProducts(productsJsonData.data)
-  }
+    const productsJsonData = await axios.get("http://localhost:3002/products")
+    console.log(productsJsonData.data)
+    setProducts(productsJsonData.data)
 
-  const addToCart = () => {
-    console.log(eachProducts)
-    selectedList.push(eachProducts)
+    for (let posts = 0; posts < products.length; posts++) {
+      const eachData = products[posts]
+      for (let keys in eachData) {
+        let eachCategory = eachData['category']
+        categorylist.push(eachCategory)
+      }
+    }
+    console.log(categorylist)
+    let categorySet = new Set(categorylist)
+    const categoriesarray = [...categorySet]
+    console.log(categoriesarray)
+    setCategories(categoriesarray)
   }
-  console.log(selectedList)
-
-  // console.log(eachProducts)
 
   return (
-    <>
-      <div className='container viewProduct'>
-        <div className="eachProductimg">
-          <img className='card' src={eachProducts.image} alt="" />
-        </div>
-        <div className="eachproductDetails">
-          <h4>{eachProducts.category}</h4>
-          <h6>{eachProducts.title}</h6>
-          <p>Price: {eachProducts.price}</p>
-          <p>Description: {eachProducts.description}</p>
-          <p>Rating rate ⭐: {eachProducts.rating.rate}</p>
-          <p>Rating count: {eachProducts.rating.count}</p>
-
-          <button className="btn btn-primary mx-2" onClick={addToCart}>Add to cart</button>
-
-          <NavLink to="/products">
-            <button className="btn btn-primary">Continue Shopping</button>
-          </NavLink>
+    <div className='MainDiv'>
+      <div className="Filter">
+        <h1>Filter</h1>
+        <h6>category</h6>
+        <div class="form-check">
+          {
+            categories.map((eachCat, index) => (
+              <>
+                <input type="checkbox" name={eachCat} />
+                <label key={index}>{eachCat}</label><br />
+              </>
+            ))
+          }
         </div>
       </div>
-      <div >
-        <SelectedProducts list={selectedList} title={"Items in Cart"} />
-      </div>
+      <div className="products">
+        {products.map((eachProduct, index) => (
+          <div className='cardBlock'>
+            <p>Product No: {index + 1}</p>
+            <div className="card productimg" style={{ width: "18rem" }}>
+              <img className='card-img-top' key={index} src={eachProduct.image} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">{eachProduct.category}</h5>
+                <p className="card-text">{eachProduct.title}</p>
+                <p className="card-text"><b>Price:</b> $ {eachProduct.price}</p>
+                {/* <NavLink to="/cart">
+                  <button className="btn btn-primary mx-2">Add to cart</button>
+                </NavLink> */}
+                <NavLink className="btn btn-primary mx-2" to={`/products/${eachProduct.id}`}>View</NavLink>
+                {/* <NavLink className="cart-sup" to="/cart">
+                  <button>
+                    <AiOutlineShoppingCart />
+                  </button>
+                </NavLink> */}
+              </div>
+            </div>
+          </div>
 
-    </>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default ViewEachProduct
+export default Products
